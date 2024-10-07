d1f2dcca3e44b283016709947cc4eedc8b1ef3d2
Arturas <xxx@example.com>
2024-10-07 11:46:00 +0300

    Fix

--- vardai.js
+++ vardai.js
@@ -1,44 +1,44 @@
 const names = [`Olivia`, `Jackson`, `Sophia`, `Elijah`, `Ava`, `Liam`, `Isabella`]
 
 
-const removeOlivia = (arr) => {
+const removeOlivia = (arr = names) => {
     const names = [...arr]
     const nameIdx = names.indexOf(`Olivia`)
     names.splice(nameIdx, 1);
     return names 
 }
 
-const removeName = (arr, name) => {
+const removeName = (arr = names, name) => {
     const names = [...arr]
     const nameIdx = names.indexOf(name)
     names.splice(nameIdx, 1);
     return names 
 }
 
-const addMason = (arr) => {
+const addMason = (arr = names) => {
     const names = [...arr]
     names.unshift(`Mason`)
     return names
 }
 
-const addNameToStart = (arr, name) => {
+const addNameToStart = (arr = names, name) => {
     const names = [...arr]
     names.unshift(name)
     return names
 }
 
-const addNameToEnd = (arr, name) => {
+const addNameToEnd = (arr = names, name) => {
     const names = [...arr]
     names.push(name)
     return names
 }
 
-const addWithBob = (arr) => [...arr].map(name => `${name}withBob`);
+const addWithBob = (arr = names) => [...arr].map(name => `${name}withBob`);
 
-const sortZA = (arr) => [...arr].sort().reverse()
+const sortZA = (arr = names) => [...arr].sort().reverse()
   
 
-console.log(removeOlivia(names));
+console.log(removeOlivia());
 console.log(removeName(names, `Olivia`));
 console.log(addMason(names));
 console.log(addNameToStart(names, `Mason`));
