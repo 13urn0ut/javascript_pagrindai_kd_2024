e5ef40548edeed3793c7bd153830036bd063bc46
Arturas <xxx@example.com>
2024-10-07 11:28:24 +0300

    Change vardai.js

--- vardai.js
+++ vardai.js
@@ -1,10 +1,24 @@
 const names = [`Olivia`, `Jackson`, `Sophia`, `Elijah`, `Ava`, `Liam`, `Isabella`]
 
+
+const removeOlivia = (arr) => {
+    const names = [...arr]
+    const nameIdx = names.indexOf(`Olivia`)
+    names.splice(nameIdx, 1);
+    return names 
+}
+
 const removeName = (arr, name) => {
     const names = [...arr]
     const nameIdx = names.indexOf(name)
     names.splice(nameIdx, 1);
     return names 
+}
+
+const addMason = (arr) => {
+    const names = [...arr]
+    names.unshift(`Mason`)
+    return names
 }
 
 const addNameToStart = (arr, name) => {
@@ -24,7 +38,9 @@ const addWithBob = (arr) => [...arr].map(name => `${name}withBob`);
 const sortZA = (arr) => [...arr].sort().reverse()
   
 
+console.log(removeOlivia(names));
 console.log(removeName(names, `Olivia`));
+console.log(addMason(names));
 console.log(addNameToStart(names, `Mason`));
 console.log(addNameToEnd(names, `Arturas`));
 console.log(addWithBob(names));
